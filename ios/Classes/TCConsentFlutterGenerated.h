d91bc57410a7228048e26c5dc44b6f04121d40a6
ahakimsaid <abdelhakim.said@commandersact.com>
2024-07-05 17:31:50 +0200

    Release mirror iab (#11)
    * update documentation
    
    * added generated header file

---
+++ ios/Classes/TCConsentFlutterGenerated.h
@@ -0,0 +1,15 @@
+//
+//  TCConsentFlutterGenerated_h.h
+//  tc_consent_plugin
+//
+//  Created by Abdelhakim SAID on 02/07/2024.
+//
+
+#ifndef TCConsentFlutterGenerated_h
+#define TCConsentFlutterGenerated_h
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const TCConsentFlutterVersion;
+
+#endif /* TCConsentFlutterGenerated_h */
